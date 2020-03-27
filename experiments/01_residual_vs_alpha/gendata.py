a56bfedd3443b8711fe35bd7734e89322d9c267c
André Costa Batista <wmwandre@gmail.com>
2020-03-27 19:21:06 -0300

    Final version
    - Fixed bugs

--- experiments/01_residual_vs_alpha/gendata.py
+++ experiments/01_residual_vs_alpha/gendata.py
@@ -6,18 +6,18 @@ import analytical as ana
 
 proportion = .5
 frequency = 1e9
-Nsources = 10
-Nsamples = 10
+Nsources = 15
+Nsamples = 15
 epsilon_rd = 2.
-Nx, Ny = 50, 50
 file_name = 'ana_1GHz_e2_d0'
 file_path = ".\data\\"
+delta = 1e-1
 
 ana.get_data(proportion=proportion,
              frequency=frequency,
              Nsources=Nsources,
              Nsamples=Nsamples,
              epsilon_rd=epsilon_rd,
-             Nx=Nx,Ny=Ny,
              file_name=file_name,
-             file_path=file_path)
+             file_path=file_path,
+             delta=delta)
--- experiments/01_residual_vs_alpha/runexp00.py
+++ experiments/01_residual_vs_alpha/runexp00.py
@@ -11,8 +11,8 @@ import inversesolvers as slv
 ## DEFINE PARAMETERS
 datafilename = 'ana_1GHz_e2_d0'
 datafilepath = '.\data\\'
-alpha = 10**(np.linspace(0,-18,50))
-# alpha = 10**(np.linspace(2,-5,20))
+resultspath = '.\\results\\'
+alpha = 10**(np.linspace(0,-18,100))
 
 # Load data
 sim = slv.BIM_Tikhonov(datafilename+'_config',model_path=datafilepath)
@@ -26,37 +26,49 @@ et = md.load_total_field(datafilename,file_path=datafilepath)
 # Load original profiles
 epsilon_r_original, _ = md.load_maps(datafilename,file_path=datafilepath)
 
-# # Run inverse solver
-# error = np.zeros(alpha.size)
-# maperror = np.zeros(alpha.size)
-# for i in range(alpha.size):
-#     epsilon_r, sigma = sim.solve_linear(es,et=et,alpha=alpha[i])
-#     error[i] = sim.compute_norm_residual(et,es,
-#                                          epsilon_r=epsilon_r,
-#                                          sigma=sigma)
-#     maperror[i] = sim.compute_map_error(epsilon_original=epsilon_r_original,
-#                                         epsilon_recovered=epsilon_r)
-#     print('alpha: %.1e - ' %alpha[i] + 'res_error: %.3e -' %error[i] 
-#           + 'map_error: %.3e' %maperror[i])
+# Run inverse solver
+error = np.zeros(alpha.size)
+maperror = np.zeros(alpha.size)
+for i in range(alpha.size):
+    epsilon_r, sigma = sim.solve_linear(es,et=et,alpha=alpha[i])
+    error[i] = sim.compute_norm_residual(et,es,
+                                         epsilon_r=epsilon_r,
+                                         sigma=sigma)
+    maperror[i] = sim.compute_map_error(epsilon_original=epsilon_r_original,
+                                        epsilon_recovered=epsilon_r)
+    print('alpha: %.1e - ' %alpha[i] + 'res_error: %.3e -' %error[i] 
+          + 'map_error: %.3e' %maperror[i])
 
-# # Plot residual results
-# plt.loglog(alpha,error,'--*')
-# plt.xlabel(r'$\alpha$')
-# plt.ylabel(r'$||y-Kx||$')
-# plt.title('Residual of equations')
-# plt.grid()
-# plt.show()
+# Plot residual results
+plt.loglog(alpha,error,'--*')
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$||y-Kx||$')
+plt.title('Residual of equations')
+plt.grid()
+plt.savefig(resultspath + 'residualconvergence.eps',format='eps')
+plt.close()
 
-# # Plot amp results
-# plt.loglog(alpha,maperror,'--*')
-# plt.xlabel(r'$\alpha$')
-# plt.ylabel(r'$\zeta_\epsilon$')
-# plt.title('Map error')
-# plt.grid()
-# plt.show()
+# Plot amp results
+plt.loglog(alpha,maperror,'--*')
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$\zeta_\epsilon$')
+plt.title('Map error')
+plt.grid()
+plt.savefig(resultspath + 'mapconvergence.eps',format='eps')
+plt.close()
 
-# Plot best solution
-# i = np.argmin(maperror)
-# sim.alpha = 1e-2 # alpha[i]
-epsilon_r, sigma = sim.solve_linear(es,et,1e-2)
-sim.plot_results(epsilon_r=epsilon_r)
+# Plot best solution - error
+i = np.argmin(error)
+epsilon_r,_ = sim.solve_linear(es,et,alpha[i])
+sim.plot_results(epsilon_r=epsilon_r,title='Best solution - residual',
+                 file_path=resultspath,file_name='best_residual')
+
+# Plot best solution - map
+i = np.argmin(maperror)
+epsilon_r,_ = sim.solve_linear(es,et,alpha[i])
+sim.plot_results(epsilon_r=epsilon_r,title='Best solution - map',
+                 file_path=resultspath,file_name='best_map')
+
+# Save data
+np.savez_compressed(resultspath+'results',alpha=alpha,
+                    residual_error=error,map_error=maperror)
