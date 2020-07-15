3acf846d4d78a1e3a0b80036b3bbe6c547aa2b90
André Costa Batista <wmwandre@gmail.com>
2020-07-15 15:58:16 -0300

    Draw random polygons

--- library_v2/experiment.py
+++ library_v2/experiment.py
@@ -5,6 +5,7 @@ Explain the module.
 
 # Standard libraries
 import numpy as np
+from numpy import random as rnd
 
 # Developed libraries
 import library_v2.error as error
@@ -90,8 +91,8 @@ def draw_square(side_length, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) + center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) + center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) + center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) + center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -180,8 +181,8 @@ def draw_triangle(side_length, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -269,8 +270,8 @@ def draw_star(side_length, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -359,8 +360,8 @@ def draw_ring(inner_radius, outer_radius, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
 
     # Set object
     epsilon_r[np.logical_and(x**2 + y**2 <= rb**2,
@@ -441,8 +442,8 @@ def draw_circle(radius, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
 
     # Set object
     epsilon_r[x**2 + y**2 <= r**2] = epsilon_ro
@@ -525,8 +526,8 @@ def draw_ellipse(x_radius, y_radius, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -611,8 +612,8 @@ def draw_cross(height, width, thickness, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -709,8 +710,8 @@ def draw_line(length, thickness, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -802,8 +803,8 @@ def draw_polygon(number_sides, radius, axis_length_x=2., axis_length_y=2.,
     dx, dy = Lx/NX, Ly/NY
 
     # Get meshgrid
-    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2-dx/2, dx) - center[1],
-                       np.arange(-Ly/2+dy/2, Ly/2-dy/2, dy) - center[0])
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
     theta = np.deg2rad(rotate)
     xp = x*np.cos(theta) + y*np.sin(theta)
     yp = -x*np.sin(theta) + y*np.cos(theta)
@@ -821,5 +822,187 @@ def draw_polygon(number_sides, radius, axis_length_x=2., axis_length_y=2.,
         polygon = np.logical_and(polygon, a*xp + b*yp >= c)
     epsilon_r[polygon] = epsilon_ro
     sigma[polygon] = sigma_o
+
+    return epsilon_r, sigma
+
+
+def isleft(x0, y0, x1, y1, x2, y2):
+    r"""Check if a point is left, on, right of an infinite line.
+
+    The point to be tested is (x2, y2). The infinite line is defined by
+    (x0, y0) -> (x1, y1).
+
+    Parameters
+    ----------
+        x0, y0 : float
+            A point within the infinite line.
+
+        x1, y1 : float
+            A point within the infinite line.
+
+        x2, y2 : float
+            The point to be tested.
+
+    Returns
+    -------
+        * float < 0, if it is on the left.
+        * float = 0, if it is on the line.
+        * float > 0, if it is on the left.
+
+    References
+    ----------
+    .. [1] Sunday, D 2012, Inclusion of a Point in a Polygon, accessed
+       15 July 2020, <http://geomalgorithms.com/a03-_inclusion.html>
+    """
+    return (x1-x0)*(y2-y0) - (x2-x0)*(y1-y0)
+
+
+def winding_number(x, y, xv, yv):
+    r"""Check if a point is within a polygon.
+
+    The method determines if a point is within a polygon through the
+    Winding Number Algorithm. If this number is zero, then it means that
+    the point is out of the polygon. Otherwise, it is within the
+    polygon.
+
+    Parameters
+    ----------
+        x, y : float
+            The point that should be tested.
+
+        xv, yv : :class:`numpy.ndarray`
+            A 1-d array with vertex points of the polygon.
+
+    Returns
+    -------
+        bool
+
+    References
+    ----------
+    .. [1] Sunday, D 2012, Inclusion of a Point in a Polygon, accessed
+       15 July 2020, <http://geomalgorithms.com/a03-_inclusion.html>
+    """
+    # The first vertex must come after the last one within the array
+    if xv[-1] != xv[0] or yv[-1] != yv[0]:
+        _xv = np.hstack((xv.flatten(), xv[0]))
+        _yv = np.hstack((yv.flatten(), yv[0]))
+        n = xv.size
+    else:
+        _xv = np.copy(xv)
+        _yv = np.copy(yv)
+        n = xv.size-1
+
+    wn = 0  # the  winding number counter
+
+    # Loop through all edges of the polygon
+    for i in range(n):  # edge from V[i] to V[i+1]
+
+        if (_yv[i] <= y):  # start yv <= y
+            if (_yv[i+1] > y):  # an upward crossing
+                # P left of edge
+                if (isleft(_xv[i], _yv[i], _xv[i+1], _yv[i+1], x, y) > 0):
+                    wn += 1  # have  a valid up intersect
+
+        else:  # start yv > y (no test needed)
+            if (_yv[i+1] <= y):  # a downward crossing
+                # P right of edge
+                if (isleft(_xv[i], _yv[i], _xv[i+1], _yv[i+1], x, y) < 0):
+                    wn -= 1  # have  a valid down intersect
+    if wn == 0:
+        return False
+    else:
+        return True
+
+
+def draw_random(number_sides, maximum_radius, axis_length_x=2.,
+                axis_length_y=2., resolution=None,
+                background_relative_permittivity=1.,
+                background_conductivity=0., object_relative_permittivity=1.,
+                object_conductivity=0., center=[0., 0.],
+                relative_permittivity_map=None, conductivity_map=None):
+    """Draw a polygon with equal sides.
+
+    Parameters
+    ----------
+        number_sides : int
+            Number of sides of the polygon.
+
+        maximum_radius : float
+            Maximum radius from the origin to each vertex.
+
+        axis_length_x, axis_length_y : float, default: 2.0
+            Length of the size of the image.
+
+        resolution : 2-tuple
+            Image resolution, in y and x directions, i.e., (NY, NX).
+            *Either this argument or relative_permittivity_map or
+            conductivity_map must be given!*
+
+        background_relative_permittivity : float, default: 1.0
+
+        background_conductivity : float, default: 0.0
+
+        object_relative_permittivity : float, default: 1.0
+
+        object_conductivity : float, default: 0.0
+
+        center : list, default: [0.0, 0.0]
+            Center of the object in the image. The center of the image
+            corresponds to the origin of the coordinates.
+
+        relative_permittivity_map : :class:`numpy.ndarray`, default:None
+            A predefined image in which the object will be drawn.
+
+        conductivity_map : :class:`numpy.ndarray`, default:None
+            A predefined image in which the object will be drawn.
+
+        rotate : float, default: 0.0 degrees
+            Rotation of the object around its center. In degrees.
+    """
+    # Make variable names more simple
+    Lx, Ly = axis_length_x, axis_length_y
+    epsilon_rb = background_relative_permittivity
+    sigma_b = background_conductivity
+    epsilon_ro = object_relative_permittivity
+    sigma_o = object_conductivity
+
+    # Set map variables
+    if relative_permittivity_map is None:
+        epsilon_r = epsilon_rb*np.ones(resolution)
+    else:
+        epsilon_r = relative_permittivity_map
+    if conductivity_map is None:
+        sigma = sigma_b*np.ones(resolution)
+    else:
+        sigma = conductivity_map
+
+    # Set discretization variables
+    if resolution is None:
+        resolution = epsilon_r.shape
+    NY, NX = resolution
+    dx, dy = Lx/NX, Ly/NY
+
+    # Get meshgrid
+    x, y = np.meshgrid(np.arange(-Lx/2+dx/2, Lx/2, dx) - center[1],
+                       np.arange(-Ly/2+dy/2, Ly/2, dy) - center[0])
+
+    # Create vertices
+    # phi = np.sort(2*np.pi*rnd.rand(number_sides))
+    phi = rnd.normal(loc=np.linspace(0, 2*np.pi, number_sides, endpoint=False),
+                     scale=0.5)
+    phi[phi >= 2*np.pi] = phi[phi >= 2*np.pi] - np.floor(phi[phi >= 2*np.pi]
+                                                   / (2*np.pi))*2*np.pi
+    phi[phi < 0] = -((np.floor(phi[phi < 0]/(2*np.pi)))*2*np.pi - phi[phi < 0])
+    phi = np.sort(phi)
+    radius = maximum_radius*rnd.rand(number_sides)
+    xv = radius*np.cos(phi)
+    yv = radius*np.sin(phi)
+
+    # Set object
+    for i in range(NX):
+        for j in range(NY):
+            if winding_number(x[j, i], y[j, i], xv, yv):
+                epsilon_r[j, i] = epsilon_ro
+                sigma[j, i] = sigma_o
 
     return epsilon_r, sigma
